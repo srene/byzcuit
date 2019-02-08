c77293877e1b68d5a80c70ff6633f596f42931ef
Mustafa Al-Bassam <mus@musalbas.com>
2019-02-08 21:50:11 +0000

    change default

--- chainspacemeasurements/chainspacemeasurements/dumper.py
+++ chainspacemeasurements/chainspacemeasurements/dumper.py
@@ -3,7 +3,7 @@ import time
 
 
 def simulation_batched(network, inputs_per_tx, outputs_per_tx):
-    num_transactions = len(network.shards)*1500
+    num_transactions = len(network.shards)*3000
     network.generate_objects(num_transactions*inputs_per_tx*5)
     network.load_objects()
     time.sleep(5)
