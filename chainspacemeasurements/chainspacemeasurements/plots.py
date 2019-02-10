b8586e223eaf9b8349891c5692daf18faade4c04
Mustafa Al-Bassam <mus@musalbas.com>
2019-02-10 14:58:17 +0000

    update plots code

--- chainspacemeasurements/chainspacemeasurements/plots.py
+++ chainspacemeasurements/chainspacemeasurements/plots.py
@@ -71,6 +71,52 @@ def plot_shard_scaling2(results1, results2, outfile):
     pyplot.close()
 
 
+def plot_shard_scaling3(results1, results2, outfile):
+    parsed_results1 = parse_shard_results(results1)
+    parsed_results2 = parse_shard_results(results2)
+    pyplot.xlabel('Number of shards')
+    pyplot.ylabel('Average transactions / second')
+    pyplot.grid(True)
+
+    pyplot.errorbar(
+        [i for i in range(2, len(parsed_results1)+2)],
+        [i[0] for i in parsed_results1],
+        [0 for i in parsed_results1],
+        marker='o',
+        color='C0',
+        label='Without defences'
+    )
+
+    pyplot.errorbar(
+        [i-0.03 for i in range(2, len(parsed_results1)+2)],
+        [i[0] for i in parsed_results1],
+        [i[1] for i in parsed_results1],
+        color='C0',
+        fmt=''
+    )
+
+    pyplot.errorbar(
+        [i for i in range(2, len(parsed_results2)+2)],
+        [i[0] for i in parsed_results2],
+        [0 for i in parsed_results2],
+        marker='s',
+        color='C1',
+        label='With defences'
+    )
+
+    pyplot.errorbar(
+        [i+0.03 for i in range(2, len(parsed_results2)+2)],
+        [i[0] for i in parsed_results2],
+        [i[1] for i in parsed_results2],
+        color='C1',
+        fmt=''
+    )
+
+    pyplot.legend(loc=4)
+    pyplot.savefig(outfile)
+    pyplot.close()
+
+
 def plot_input_scaling(results, outfile):
     parsed_results = parse_shard_results(results)
     pyplot.xlabel('Number of inputs per transaction')
@@ -134,6 +180,10 @@ if __name__ == '__main__':
         results1 = json.loads(open(sys.argv[2]).read())
         results2 = json.loads(open(sys.argv[3]).read())
         plot_shard_scaling2(results1, results2, sys.argv[4])
+    elif sys.argv[1] == 'shardscaling3':
+        results1 = json.loads(open(sys.argv[2]).read())
+        results2 = json.loads(open(sys.argv[3]).read())
+        plot_shard_scaling3(results1, results2, sys.argv[4])
     elif sys.argv[1] == 'inputscaling':
         results = json.loads(open(sys.argv[2]).read())
         plot_input_scaling(results, sys.argv[3])
