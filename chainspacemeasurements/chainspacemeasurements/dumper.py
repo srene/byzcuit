62ee26bfb0be4699443e67bba14c6ea9b222e970
Mustafa Al-Bassam <mus@musalbas.com>
2019-02-13 16:42:45 +0000

    Update dumper.py

--- chainspacemeasurements/chainspacemeasurements/dumper.py
+++ chainspacemeasurements/chainspacemeasurements/dumper.py
@@ -5,7 +5,10 @@ import time
 def simulation_batched(network, inputs_per_tx, outputs_per_tx, num_transactions=None, batch_size=4000, batch_sleep=1, input_object_mode=0, create_dummy_objects=0, num_dummy_objects=0, output_object_mode=0):
     if num_transactions is None:
         num_transactions = len(network.shards)*6000
-    network.generate_objects(num_transactions*inputs_per_tx*5)
+    if num_dummy_objects != 0:
+        network.generate_objects(num_transactions*(inputs_per_tx+num_dummy_objects)*5)
+    else:
+        network.generate_objects(num_transactions*inputs_per_tx*5)
     network.load_objects()
     time.sleep(5)
 
