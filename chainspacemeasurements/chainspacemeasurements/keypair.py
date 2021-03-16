b677b21af26fe5c9498c83c97899ce9787fe8a98
sergi <sergi@srenevic.net>
2021-03-16 12:07:54 +0100

    keypair generator

---
+++ chainspacemeasurements/chainspacemeasurements/keypair.py
@@ -0,0 +1,13 @@
+import boto3
+ec2 = boto3.resource('ec2')
+
+# create a file to store the key locally
+outfile = open('ec2-keypair2.pem','w')
+
+# call the boto ec2 function to create a key pair
+key_pair = ec2.create_key_pair(KeyName='ec2-keypair2')
+
+# capture the key and store it in a file
+KeyPairOut = str(key_pair.key_material)
+print(KeyPairOut)
+outfile.write(KeyPairOut)
